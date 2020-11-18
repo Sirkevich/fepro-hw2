ae0f8b2311a63e3ab18f2d95bfa4a2e45b15fe74
Ihor SS <62531809+Sirkevich@users.noreply.github.com>
2020-11-18 22:06:38 +0200

    add age rules

---
+++ app.js
@@ -0,0 +1,27 @@
+const currentYear = +2020;
+
+let userYearOfBirth = +prompt('Введите год Вашего рождения');
+
+if (userYearOfBirth > 0 && userYearOfBirth < currentYear) {
+
+    let userAge = currentYear - userYearOfBirth;
+    console.log('Ваш возраст', +userAge);
+
+    if (userAge >= 18) {
+        console.log('Добро пожаловать на наш сайт !')
+
+    } else if (userAge <= 12) {
+        console.log('Извини, но мы тебя не пропустим, посети learn.javascript.ru и узнаешь как сюда попасть');
+    } else {
+        console.log('Извини, но тебе нет 18 :( Когда исполнится - заходи в любое время, мы будем ждать');
+    }
+
+} else if (userYearOfBirth > currentYear) {
+    console.log('Ти родишся в', userYearOfBirth + ' году')
+} else if (userYearOfBirth <= 0) {
+    console.log('Указан некоректный год');
+} else {
+    (userYearOfBirth !== Number)
+    console.log("Введите числовое значение!");
+}
+
