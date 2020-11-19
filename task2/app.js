a4f7b374a37aee83bb2eb99a52a3ffecdd1abfde
Ihor SS <62531809+Sirkevich@users.noreply.github.com>
2020-11-19 22:51:00 +0200

    add conditions

--- task2/app.js
+++ task2/app.js
@@ -2,43 +2,47 @@ const currentYear = +2020;
 
 let userName = prompt('Добро пожаловать к нам на сайт! Введите Ваше имя');
 
-    if (userName == false){
-        let companyQuestion = prompt('Введите Ваше имя');
-    } else {
-        console.log('Привет', userName);
-        let userYearOfBirth = +prompt('Введите год Вашего рождения');
-       
-        if (userYearOfBirth > 0 && userYearOfBirth < currentYear) {
+if (userName != +userName) {
+    alert(userName + ', ' + 'рады знакомству, мы компания Lohika');
+    let userYearOfBirth = +prompt('Введите год Вашего рождения');
 
-            let userAge = currentYear - userYearOfBirth;
-       
-            if (userAge >= 24 && userAge <= 32 )   {
-               companyQuestion = confirm('У Вас есть опыт работы в сфере Frontend разработки?');
+    if (userYearOfBirth > 0 && userYearOfBirth < currentYear) {
+        let userAge = currentYear - userYearOfBirth;
 
-                if (companyQuestion == true) {
-                    companyQuestion = +prompt('Сколько лет Вы работаете в этой сфере');
+        if (userAge >= 24 && userAge <= 32) {
+            companyQuestion = confirm('У Вас есть опыт работы в сфере Frontend разработки?');
+
+            if (companyQuestion == true) {
+                let userExpirience = +prompt('Сколько лет Вы работаете в этой сфере');
+
+                if (userExpirience >= 2) {
+                    let userEnglishLevel = prompt('Прекрасно, укажите Ваш уровень английского: pre-intermediate, intermediate, upper intermediate?');
+
+                    if (userEnglishLevel == 'intermediate' || userEnglishLevel == 'upper-intermediate') {
+                        let personalUserProjects = confirm('Прекрасно, у Вас есть персональные проекты?');
+
+                        if (personalUserProjects == true) {
+                            let companyAnswer = alert('Хорошо, вышлите вашы проекты на email:job_odessa@lohika.com\nРады знакомству, мы с Вами свяжемся в ближайшее время!!');
+                        } else {
+                            companyAnswer = confirm('Извените, но нам нужны опытные разработчики');
+                        }
+                    } else {
+                        companyAnswer = confirm('Извените, но нам нужны люди с мимальным уровнем английского Intermediate');
+                    }
+
                 } else {
-                    let companyAnswer = alert('Извените, но нам нужны люди с опытом работы') 
+                    companyAnswer = alert('Извените, но нам нужны люди с опытом работы не менее 2 лет')
                 }
-    
-            } else if (userYearOfBirth > currentYear) {
-                console.log('Ти родишся в', userYearOfBirth + ' году')
-            } else if (userYearOfBirth <= 0) {
-                console.log('Указан некоректный год');
-            } else if (userYearOfBirth !== Number){
-                
-                console.log("Введите числовое значение!");
-            
-            
+            } else {
+                companyAnswer = alert('Извените, но нам нужны люди с опытом работы')
             }
-            
-            else {
-                let companyAnswer = alert('Извените, но Вы нам не подходите по возрастным ограничениям');
-            }
+
+        } else {
+            let companyAnswer = alert('Извените, но Вы нам не подходите по возрастным ограничениям');
+        }
+    } else {
+        let companyQuestion = alert('Вы ввели некоректное значение, обновите станицу и начните сначала');
     }
-    
-
- 
-
-
+} else {
+    let companyQuestion = alert('Вы не ввели имя, обновите станицу и начните сначала');
 }
