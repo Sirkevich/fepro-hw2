bcb12c58b946b6153378b1885b8ff93cfe11d68a
Ihor SS <62531809+Sirkevich@users.noreply.github.com>
2020-11-20 01:34:24 +0200

    do task1

--- task1/app.js
+++ task1/app.js
@@ -7,15 +7,10 @@ if (userYearOfBirth > 0 && userYearOfBirth < currentYear) {
     let userAge = currentYear - userYearOfBirth;
     console.log('Ваш возраст', +userAge);
 
-    if (userAge >= 18) {
-        console.log('Добро пожаловать на наш сайт !')
+    userAge = (userAge >= 18) ? console.log('Добро пожаловать на наш сайт !') : (userAge <= 12) ? console.log('Извини, но мы тебя не пропустим, посети learn.javascript.ru и узнаешь как сюда попасть') : console.log('Извини, но тебе нет 18 :( Когда исполнится - заходи в любое время, мы будем ждать');
 
-    } else if (userAge <= 12) {
-        console.log('Извини, но мы тебя не пропустим, посети learn.javascript.ru и узнаешь как сюда попасть');
-    } else {
-        console.log('Извини, но тебе нет 18 :( Когда исполнится - заходи в любое время, мы будем ждать');
-    }
-
+} else if (userYearOfBirth === currentYear) {
+    console.log('Тебе меньше года');
 } else if (userYearOfBirth > currentYear) {
     console.log('Ти родишся в', userYearOfBirth + ' году')
 } else if (userYearOfBirth <= 0) {
